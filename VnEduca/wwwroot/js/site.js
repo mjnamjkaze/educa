dd3b5eed02b4148591252e7fc19ae4c4061361a0
NamVH <mjnamjkaze@gmail.com>
2019-11-17 21:52:33 +0700

    add templates 2019/11/17

---
+++ VnEduca/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $("#testMenu").click(function (e) {
+        doShowMenu();
+    })
+})
+
+
+function doShowMenu() {
+ 
+    $(".app-drawer").addClass("opened")
+
+    $(".app-drawer-scrim").click(function () {
+        $(this).closest(".app-drawer").removeClass("opened")
+    })
+}
